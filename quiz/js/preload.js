549ce702342af89f601b76a86217d270cf1dbf96
Dhaneshkumar S <114380354+dkcyberz@users.noreply.github.com>
2024-06-12 11:17:25 +0530

    Delete quiz/js directory

--- quiz/js/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electron', {
-  send: (channel, data) => ipcRenderer.send(channel, data),
-  receive: (channel, func) => ipcRenderer.on(channel, (event, ...args) => func(...args)),
-  invoke: (channel, data) => ipcRenderer.invoke(channel, data) // For using ipcRenderer.invoke
-});
